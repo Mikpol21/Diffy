ea0df8620171268cdc3bef2b530b9c2ef557af82
Mikpol21 <mikolajpolinski21@gmail.com>
2023-03-30 17:27:32 +0200

    First attempt failed

--- dual_numbers.hs
+++ dual_numbers.hs
@@ -1,6 +1,6 @@
 module Dual where
 import Expr (Doubleable(..), fromDouble, Evaluable(..))
-data Dual a =  Const Double | Dual Double a
+data Dual a =  Const Double | Dual a a
   deriving (Eq)
 
 instance Doubleable a => Doubleable (Dual a) where
@@ -10,18 +10,18 @@ instance Evaluable a => Evaluable (Dual a)
 
 instance Show a => Show (Dual a) where
     show (Const x) = show x
-    show (Dual x y) = show x ++ " + " ++ show y ++ "ε"
+    show (Dual x y) =  "(" ++ show x ++ " + " ++ show y ++ "ε" ++ ")"
 
 instance (Doubleable a, Num a) => Num (Dual a) where
     (Const x) + (Const y) = Const (x + y)
-    (Const x) + (Dual y z) = Dual (x + y) z
-    (Dual x y) + (Const z) = Dual (x + z) y
+    (Const x) + (Dual y z) = Dual (fromDouble x + y) z
+    (Dual x y) + (Const z) = Dual (x + fromDouble z) y
     (Dual x y) + (Dual z w) = Dual (x + z) (y + w)
 
     (Const x) * (Const y) = Const (x * y)
-    (Const x) * (Dual y z) = Dual (x * y) (fromDouble x * z)
-    (Dual x y) * (Const z) = Dual (x * z) (y * fromDouble z)
-    (Dual x y) * (Dual z w) = Dual (x * z) (fromDouble x * w + y * fromDouble z)
+    (Const x) * (Dual y z) = Dual (fromDouble x * y) (fromDouble x * z)
+    (Dual x y) * (Const z) = Dual (x * fromDouble z) (y * fromDouble z)
+    (Dual x y) * (Dual z w) = Dual (x * z) (x * w + y * z)
 
     negate (Const x) = Const (negate x)
     negate (Dual x y) = Dual (negate x) (negate y)
@@ -36,30 +36,30 @@ instance (Doubleable a, Num a) => Num (Dual a) where
 
 instance (Doubleable a, Fractional a) => Fractional (Dual a) where
     (Const x) / (Const y) = Const (x / y)
-    (Const x) / (Dual y z) = Dual (x / y) (negate (fromDouble x / (fromDouble y * fromDouble y)) * z)
-    (Dual x y) / (Const z) = Dual (x / z) (y / fromDouble z)
-    (Dual x y) / (Dual z w) = Dual (x / z) ((y * fromDouble z - fromDouble x * w) / (fromDouble z * fromDouble z))
+    (Const x) / (Dual y z) = Dual (fromDouble x / y) (negate (fromDouble x / (y * y)) * z)
+    (Dual x y) / (Const z) = Dual (x / fromDouble z) (y / fromDouble z)
+    (Dual x y) / (Dual z w) = Dual (x / z) ((y * z - x * w) / (z * z))
 
     fromRational x = Const (fromRational x)
 
 instance (Doubleable a, Floating a) => Floating (Dual a) where
     pi = Const pi
     exp (Const x) = Const (exp x)
-    exp (Dual x y) = Dual (exp x) (y * exp (fromDouble x))
+    exp (Dual x y) = Dual (exp x) (y * exp (x))
     log (Const x) = Const (log x)
-    log (Dual x y) = Dual (log x) (y / fromDouble x)
+    log (Dual x y) = Dual (log x) (y / x)
     sin (Const x) = Const (sin x)
-    sin (Dual x y) = Dual (sin x) (y * fromDouble (cos x))
+    sin (Dual x y) = Dual (sin x) (y * (cos x))
     cos (Const x) = Const (cos x)
-    cos (Dual x y) = Dual (cos x) (negate (y * fromDouble (sin x)))
+    cos (Dual x y) = Dual (cos x) (negate (y * (sin x)))
     sinh (Const x) = Const (sinh x)
-    sinh (Dual x y) = Dual (sinh x) (y * fromDouble (cosh x))
+    sinh (Dual x y) = Dual (sinh x) (y * (cosh x))
     cosh (Const x) = Const (cosh x)
-    cosh (Dual x y) = Dual (cosh x) (y * fromDouble (sinh x))
+    cosh (Dual x y) = Dual (cosh x) (y * (sinh x))
     asinh (Const x) = Const (asinh x)
-    asinh (Dual x y) = Dual (asinh x) (y / fromDouble (sqrt (1 + x * x)))
+    asinh (Dual x y) = Dual (asinh x) (y / (sqrt (1 + x * x)))
     acosh (Const x) = Const (acosh x)
-    acosh (Dual x y) = Dual (acosh x) (y / fromDouble (sqrt (x * x - 1)))
+    acosh (Dual x y) = Dual (acosh x) (y / (sqrt (x * x - 1)))
     atanh (Const x) = Const (atanh x)
-    atanh (Dual x y) = Dual (atanh x) (y / (fromDouble (sqrt (1 - x * x))))
+    atanh (Dual x y) = Dual (atanh x) (y / ((sqrt (1 - x * x))))
 
